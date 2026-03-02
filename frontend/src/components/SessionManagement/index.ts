aa2f7e91e8d70adeb137a20fa02852ec27b07d85
Adrian Wedd <adrian@adrianwedd.com>
2026-03-02 23:10:56 +1100

    chore: update barrel export with SessionControls, TargetMemory, SessionSummary

--- frontend/src/components/SessionManagement/index.ts
+++ frontend/src/components/SessionManagement/index.ts
@@ -2,3 +2,7 @@ export { PhaseIndicator } from './PhaseIndicator';
 export { SUDScale } from './SUDScale';
 export { VOCScale } from './VOCScale';
 export { SessionTimer } from './SessionTimer';
+export { SessionControls } from './SessionControls';
+export { TargetMemoryForm } from './TargetMemoryForm';
+export { TargetMemoryCard } from './TargetMemoryCard';
+export { SessionSummary } from './SessionSummary';
